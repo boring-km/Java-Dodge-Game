318e33c4bb015df3366b7afc658f627ec7b5d0d8
boring-km <kms0644804@naver.com>
2021-04-26 22:12:29 +0900

    [master] 눈에 보이는 지저분한 부분 먼저 리팩토링

--- Dodge/src/GameStart.java
+++ Dodge/src/GameStart.java
@@ -1,99 +1,70 @@
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Point;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+public class GameStart extends JFrame {
 
-public class GameStart extends JFrame{
+	@Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Image img = Toolkit.getDefaultToolkit().getImage("src/File/Main1.png");
+        g.drawImage(img, 297, 300, this);
+    }
 
-	private BufferedImage img;
-	private GameScore GSC;
-	private Garo spaceEffect;		// 우주효과
-	private Back back;				// 게임 플레이화면
+    public GameStart() {
+        setLayout(null);
+        setBounds(50, 100, 1600, 900);
+        setUndecorated(true);
+        setBackground(new Color(0, 0, 0, 0));
 
-	public void paint(Graphics g) {
+        ImageIcon normalStartIcon = new ImageIcon("src/File/start.png");            // 게임시작 버튼
+        ImageIcon rolloverStartIcon = new ImageIcon("src/File/start1.png");
+        ImageIcon pressedStartIcon = new ImageIcon("src/File/start2.png");
 
-		super.paint(g);
-		Image img = Toolkit.getDefaultToolkit().getImage("src/File/Main1.png");
-		g.drawImage(img, 297, 300, this);
-
-	}
-	public GameStart() {
-
-		setLayout(null);
-		setBounds(50,100,1600,900);
-		setUndecorated(true);
-		setBackground(new Color(0,0,0,0));
-
-		ImageIcon Nstart = new ImageIcon("src/File/start.png");			// 게임시작 버튼
-		ImageIcon Rstart = new ImageIcon("src/File/start1.png");
-		ImageIcon Pstart = new ImageIcon("src/File/start2.png");
-
-		JButton start = new JButton(Nstart);
-		start.setPressedIcon(Pstart);
-		start.setRolloverIcon(Rstart);
-		start.setBorderPainted(false);
-		start.setBounds(160,650,427,140);
-
-		ImageIcon Nscore = new ImageIcon("src/File/score.png");			// 점수보기 버튼
-		ImageIcon Rscore = new ImageIcon("src/File/score1.png");
-		ImageIcon Pscore = new ImageIcon("src/File/score2.png");
-
-		JButton score = new JButton(Nscore);
-		score.setPressedIcon(Pscore);
-		score.setRolloverIcon(Rscore);
-		score.setBorderPainted(false);
-		score.setBounds(587,650,427,140);
-
-		ImageIcon Nquit = new ImageIcon("src/File/quit.png");			// 종료버튼
-		ImageIcon Rquit = new ImageIcon("src/File/quit1.png");
-		ImageIcon Pquit = new ImageIcon("src/File/quit2.png");
-
-		JButton quit = new JButton(Nquit);
-		quit.setPressedIcon(Pquit);
-		quit.setRolloverIcon(Rquit);
-		quit.setBorderPainted(false);
-		quit.setBounds(1014,650,427,140);
-		quit.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
+        JButton start = new JButton(normalStartIcon);
+        start.setPressedIcon(pressedStartIcon);
+        start.setRolloverIcon(rolloverStartIcon);
+        start.setBorderPainted(false);
+        start.setBounds(160, 650, 427, 140);
+		start.addActionListener(e -> {
+			dispose();
+			new Back();
 		});
-		start.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
 
-				dispose();
-				back = new Back();
+        ImageIcon normalScoreIcon = new ImageIcon("src/File/score.png");            // 점수보기 버튼
+        ImageIcon rolloverScoreIcon = new ImageIcon("src/File/score1.png");
+        ImageIcon pressedScoreIcon = new ImageIcon("src/File/score2.png");
 
-			}
+        JButton score = new JButton(normalScoreIcon);
+        score.setPressedIcon(pressedScoreIcon);
+        score.setRolloverIcon(rolloverScoreIcon);
+        score.setBorderPainted(false);
+        score.setBounds(587, 650, 427, 140);
+		score.addActionListener(e -> {
+			dispose();
+			new GameScore("", 0, 0, null);
 		});
-		score.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
 
-				dispose();
-				GSC = new GameScore("", 0, 0, null);
+        ImageIcon normalQuitIcon = new ImageIcon("src/File/quit.png");            // 종료버튼
+        ImageIcon rolloverQuitIcon = new ImageIcon("src/File/quit1.png");
+        ImageIcon pressedQuitIcon = new ImageIcon("src/File/quit2.png");
 
-			}
-		});
-		//마우스 커서
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		Cursor invisCursor = tk.createCustomCursor(tk.createImage("src/File/Yellow.png"), new Point(), null);
-		setCursor(invisCursor);
-		getGlassPane().setVisible(true);
-		//마우스 커서
+        JButton quit = new JButton(normalQuitIcon);
+        quit.setPressedIcon(pressedQuitIcon);
+        quit.setRolloverIcon(rolloverQuitIcon);
+        quit.setBorderPainted(false);
+        quit.setBounds(1014, 650, 427, 140);
+        quit.addActionListener(e -> System.exit(0));
 
-		add(start);
-		add(score);
-		add(quit);
-		setVisible(true);
-	}
+        //마우스 커서
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Cursor invisibleCursor = tk.createCustomCursor(tk.createImage("src/File/Yellow.png"), new Point(), null);
+        setCursor(invisibleCursor);
+        getGlassPane().setVisible(true);
 
+        // 버튼 추가
+        add(start);
+        add(score);
+        add(quit);
+        setVisible(true);
+    }
 }
